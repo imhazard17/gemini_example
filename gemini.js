669165e82d56bc119bb61afed052e7aedf5b8ba8
Rajarshi Barman <rajubarman4635@gmail.com>
2024-07-31 16:10:03 +0530

    added comments

--- gemini.js
+++ gemini.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 require('dotenv').config()
 const mime = require('mime-types')
 
+// object of prompts
 const prompts = {
     "image": `Please provide type of report uploaded, technique, diagnosis details, findings, clinical indication and
     impression for this medical report also also provide medical report name for each of the files which are images only. Reply with only JSON in the JSON format specified below: {"Findings":"Findings of type
@@ -23,6 +24,7 @@ const prompts = {
     specified below: {"Summary":"Summary of type Text"}`
 }
 
+// referred from the docs: https://ai.google.dev/gemini-api/docs/get-started/tutorial?lang=node#generate-text-from-text-and-image-input
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 function fileToGenerativePart(path, mimeType) {
@@ -39,33 +41,37 @@ async function run() {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const chat = model.startChat()
 
+    // make list of GoogleGenerativeAI.Part objects to upload files with prompts
     const fileParts = []
     const files = fs.readdirSync('./docs')
     files.forEach(file => fileParts.push(fileToGenerativePart(`docs/${file}`, mime.lookup(file))))
 
+    // upload only images for image prompt and store response in docWiseReport
     let result = await chat.sendMessage([prompts['image'], ...fileParts.filter(part => part.inlineData.mimeType.startsWith('image'))]);
     let response = await result.response;
     let docWiseReport = response.text();
 
-
+    // upload only pdf for pdf prompt and concatinate its response to docWiseReport so it contain combined response of image and pdf prompt
     result = await chat.sendMessage([prompts['pdf'], ...fileParts.filter(part => part.inlineData.mimeType.endsWith('pdf'))]);
-    response = await result.response;
+    response = result.response;
 
     docWiseReport += response.text()
+    console.log('============================DOCUMENT WISE REPORTS===============================')
     console.log(docWiseReport);
-    console.log('----------------------------------------------------------------------------------------')
     console.log('')
 
+    // upload all files (both pdfs and images) to treatment and summary prompts
     result = await chat.sendMessage([prompts['treatment'], ...fileParts]);
-    response = await result.response;
+    response = result.response;
     const treatmentDetails = response.text();
+    console.log('===========================TREATMENT DETAILS===========================')
     console.log(treatmentDetails);
-    console.log('----------------------------------------------------------------------------------------')
     console.log('')
 
     result = await chat.sendMessage([prompts['summary'], ...fileParts]);
-    response = await result.response;
+    response = result.response;
     const summary = response.text();
+    console.log('========================SUMMARY=============================')
     console.log(summary);
 }
 
